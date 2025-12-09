227fcec67f414b6917abca64cf019c0bd98b790d
HumaidAlzdjali <60912279+HumaidAbdul@users.noreply.github.com>
2025-12-10 00:04:02 +0400

    first commit-3

--- src/lib/api.js
+++ src/lib/api.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const API_BASE_URL = "https://interlinkuae.onrender.com";
 
 export const api = axios.create({
-  baseURL: API_BASE_URL + "/api",
+  baseURL: API_BASE_URL
 });
 
 // after creating the axios instance
